386e53cec24eb5308cd7ef47efa5baefabbc7663
Daniel Rippel <83886937+DanTheBow@users.noreply.github.com>
2022-01-02 20:57:11 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Geburtstagskarten Generator:")
+print()
+
+name = input("Name der zu grüßenden Person eingeben: ")
+age = input("Alter der zu grüßenden Person eingeben: ")
+sender = input("Eigenen Namen eingeben: ")
+
+print("Hallo " + name + ",")
+print("Ich wünsche dir alles Gute zum " + age + "ten Geburtstag.")
+print("Ich hoffe das du einen schönen Tag hast und mit deinen liebsten feierst.")
+print("Zudem wünsche ich dir alles nur erdenkliche Gute für das neue Lebensjahr und viel Gesundheit!")
+print("Liebe Grüße")
+print(sender)
